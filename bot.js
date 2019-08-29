4847c3523555d38ba50f57999c010e38095a82c1
KaminoShimobe <AbdulKirby@gmail.com>
2019-08-29 14:09:48 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,97 @@
+const Discord = require("discord.js");
+const Danbooru = require('danbooru');
+const mysql = require("mysql");
+const http = require('http');
+const pixel = require('pixel-art');
+const dailyCD = new Set();
+const raceCD = new Set();
+const commandCD = new Set();
+
+const bot = new Discord.Client({disableEveryone: true})
+
+
+var con_fig = {
+	host: "us-cdbr-iron-east-01.cleardb.net",
+	user: "bc9ba9370a9522",
+	password: process.env.MY_SQL,
+	database: "heroku_b523f37d8e76acb",
+	port: 3306
+};
+
+var con;
+
+function handleDisconnect() {
+con = mysql.createConnection(con_fig);
+con.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  }); 	
+
+process.on('uncaughtException', function (err) {
+    console.log(err);
+	
+}); 
+	
+
+
+con.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+       throw err;                                 // server variable configures this)
+    }
+});
+       }
+
+
+
+handleDisconnect();
+
+bot.on("ready", async () => {
+
+	console.log(`Bot is ready bois! ${bot.user.username}`);
+  bot.user.setPresence({ status: 'online', game: { name: '&help'} });
+  
+  try {
+
+		let link = await bot.generateInvite(["ADMINISTRATOR"]);
+
+		console.log(link);
+
+	}	catch(e) {
+
+		console.log(e.stack);
+
+	}
+
+
+
+});
+
+bot.on("message", async message => {
+	
+	let messageArray = message.content.split(" ");
+
+	let command = messageArray[0];
+
+	let args = messageArray.slice(1);
+
+	
+	
+
+
+	
+
+
+		
+	
+	
+	if(message.author.bot) return;
+  
+  });
+
+
+bot.login(process.env.BOT_TOKEN);
