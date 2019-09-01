a4758ee0d4284635f0dafc396e929bcc4e3daf19
KaminoShimobe <AbdulKirby@gmail.com>
2019-09-01 11:58:12 -0500

    User functions

--- bot.js
+++ bot.js
@@ -121,6 +121,8 @@ bot.on("message", async message => {
 	if(message.author.bot) return;
 		
 	if(message.channel.type === "dm") return;	
+//functions
+	
 	
 	function help(){
 
@@ -134,11 +136,319 @@ bot.on("message", async message => {
 		message.author.sendEmbed(help);
 		message.reply(" sent you a dm of the help list!");
 }
+	
+function addUser(){
+		
+		con.query(`SELECT * FROM user WHERE id = '${message.author.id}'`, (err, rows) => {
+		if(err) throw err;
+		let sql;
+		if(rows.length < 1) {
+			
+			sql = `INSERT INTO user (id, money, level, exp, bio, inventory, streak, stables, horses, hue) VALUES ('${message.author.id}', ${0}, ${1}, ${0}, '&bio to set your bio', '', ${4}, ${0}, '', '#942906')`;
+			con.query(sql, console.log);
+			message.channel.send(`User account created! ${prefix}view to view your account!`)
+			return;
+		}	else {
+
+			message.reply(` You have a user! Do ${prefix}view to see your user`);
+			
+
+			
+			return;
+		}
+
+
+		});
+		
+	}
+	
+	function viewUser(){
+		
+con.query(`SELECT * FROM user WHERE id = '${message.author.id}'`, (err, rows) => {
+		if(err) throw err;
+
+		if(rows.length < 1) {
+			message.reply(`You have no user! \n Type ${prefix}user to create one!`);
+			
+			return;
+		}
+
+		let money = rows[0].money;
+		let bio = rows[0].bio;
+		let level = rows[0].level;
+		let color = rows[0].hue;
+		let exp = rows[0].exp;
+		let streak = rows[0].streak;
+		let stables = rows[0].stables;
+		let horses = rows[0].horses;
+		var cap = level * 100;
+		var meter = ""
+		var percent = exp / cap;
+		
+for(var i = 0; i < 100; i++){
+	if(i <= percent){
+		meter += "-";
+	} else {
+		meter += ".";
+	}	
+}
+		
+var PixelArt = require('pixel-art');	
+const { createCanvas } = require('canvas')	
+		const mycanvas = createCanvas(100, 1)	
+	var artwork = PixelArt.art(meter)
+  .palette({
+    '.': '#03a619',
+    '-': '#00ff22'
+  })
+  .pos({ x: 0, y: 0 })
+  .scale(4)
+  .draw(mycanvas.getContext('2d'));		
+	
+var art = mycanvas.toBuffer() // defaults to PNG
+var fileName = message.author.username + "-32-art.png";
+const artPiece = new Discord.Attachment(art, fileName);
+
+		
+
+		let stats = new Discord.RichEmbed()
+
+			
+			.setAuthor(message.author.username)
+			.attachFile(artPiece)
+			.setDescription("Level: " + level + "\n EXP: " + exp + "/" + cap + "\n Carrots: :carrot:" + money + "\n" + bio + "\n + Horses: " + horses + "\n Stables: " + stables)
+			.setFooter("ID:" + message.author.id, message.author.avatarURL)
+			.setColor(color); 
+
+		message.channel.sendEmbed(stats);
+
+
+		
+		
+
+	});
+
+}
+	
+function viewOtherUser(){
+		let other = message.mentions.users.first();
+con.query(`SELECT * FROM user WHERE id = '${other.id}'`, (err, rows) => {
+		if(err) throw err;
+
+		if(rows.length < 1) {
+			message.reply(`You have no user! \n Type ${prefix}user to create one!`);
+			
+			return;
+		}
+
+		let money = rows[0].money;
+		let bio = rows[0].bio;
+		let level = rows[0].level;
+		let color = rows[0].hue;
+		let exp = rows[0].exp;
+		let streak = rows[0].streak;
+		let stables = rows[0].stables;
+		let horses = rows[0].horses;
+		var cap = level * 100;
+		var meter = ""
+		var percent = exp / cap;
+		
+for(var i = 0; i < 100; i++){
+	if(i <= percent){
+		meter += "-";
+	} else {
+		meter += ".";
+	}	
+}
+		
+var PixelArt = require('pixel-art');	
+const { createCanvas } = require('canvas')	
+		const mycanvas = createCanvas(100, 1)	
+	var artwork = PixelArt.art(meter)
+  .palette({
+    '.': '#03a619',
+    '-': '#00ff22'
+  })
+  .pos({ x: 0, y: 0 })
+  .scale(4)
+  .draw(mycanvas.getContext('2d'));		
+	
+var art = mycanvas.toBuffer() // defaults to PNG
+var fileName = message.author.username + "-32-art.png";
+const artPiece = new Discord.Attachment(art, fileName);
+
+		
+
+		let stats = new Discord.RichEmbed()
+
+			
+			.setAuthor(message.author.username)
+			.attachFile(artPiece)
+			.setDescription("Level: " + level + "\n EXP: " + exp + "/" + cap + "\n Carrots: :carrot:" + money + "\n" + bio + "\n + Horses: " + horses + "\n Stables: " + stables)
+			.setFooter("ID:" + other.id, other.avatarURL)
+			.setColor(color); 
+
+		message.channel.sendEmbed(stats);
+
+
+		
+		
+
+	});
+
+}	
+	
+function deleteUser(){
+
+con.query(`SELECT * FROM user WHERE id = '${message.author.id}'`, (err, rows) => {
+		if(err) throw err;
+
+		let sql;
+		if(rows.length < 1) {
+			message.reply(`You have no user! \n Type ${prefix}user to create one!`);
+			
+			return;
+		} else {
+			sql = `DELETE FROM user WHERE id = '${message.author.id}'`;
+			con.query(sql, console.log);
+			message.reply(`User Deleted! ${prefix}user to create a new one!`);
+		}
+
+	});
+	return;
+}	
+	
+function bio(){
+
+	
+
+con.query(`SELECT * FROM user WHERE id = '${message.author.id}'`, (err, rows) => {
+		if(err) throw err;
+
+		if(rows.length < 1) {
+			message.reply("You have no user! Type &help for a list of commands!");
+			
+			return;
+		}
+
+		
+		let bio = rows[0].bio;
+		
+				
+		if(message.channel.type === "dm"){
+			message.author.send("Update your bio! You have 100 characters. \n &cancel to cancel.");
+		} else {
+			message.channel.send("Update your bio! You have 100 characters. \n &cancel to cancel.");
+		}
+		const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 100000000 });
+        		collector.once('collect', message => {
+            		if (message.content == `&cancel`) {
+               		 message.channel.send("Message cancelled.");
+                		return;
+            		} else {
+				
+				sql = `UPDATE user SET bio = "${message.content}" WHERE id = '${message.author.id}'`;
+				con.query(sql);
+				if(message.channel.type === "dm"){
+			message.author.send("Bio Updated!");
+		} else {
+			message.channel.send("Bio Updated!");
+		}
+			}
+			});
+
+		
+		
+
+	});
+				
+}		
+
+function hexcolor(){
+
+
+con.query(`SELECT * FROM user WHERE id = '${message.author.id}'`, (err, rows) => {
+		if(err) throw err;
+
+		if(rows.length < 1) {
+			message.reply("You have no user! Type &help for a list of commands!");
+			
+			return;
+		}
+
+		
+		let color = rows[0].hue;
+		
+		if(message.channel.type === "dm"){
+			message.author.send("Update your profile color! Send the hexidecimal for your profile. \n &cancel to cancel.");
+		} else {
+			message.channel.send("Update your profile color! Send the hexidecimal for your profile. \n &cancel to cancel.");
+		}		
+
+		
+		const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 100000000 });
+        		collector.once('collect', message => {
+            		if (message.content == `&cancel`) {
+               		 message.channel.send("Message cancelled.");
+                		return;
+            		} else {
+				
+				sql = `UPDATE user SET hue = '${message.content}' WHERE id = '${message.author.id}'`;
+				con.query(sql);
+				message.channel.send("Color Updated!");
+			}
+			});
+
+		
+		
+
+	});
+
+}		
+	
+//commands	
 
 	if(command === `${prefix}help`){
 		help();
 }
 	
+if(command === `${prefix}user`){
+		addUser();
+}	
+	
+	if(command === `${prefix}bio`){
+		
+		
+
+		bio();
+	
+
+		 return; 	
+
+} 
+
+if(command === `${prefix}color`){
+		
+
+		hexcolor();
+	
+
+		 return; 	
+
+}
+	
+if(command === `${prefix}view` && messageArray[1] === undefined){
+	viewUser();
+}
+	
+if(command === `${prefix}view` && messageArray[1] != undefined ){	
+	viewOtherUser();
+}	
+	
+if(command === `${prefix}delete`){
+	deleteUser();
+}	
+
   
   });
 
